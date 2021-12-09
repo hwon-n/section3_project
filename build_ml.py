965b5c83623c910223dd1a725b8edb8a0096b3a3
[hwon-n] <woo_oas@naver.com>
2021-12-09 23:34:49 +0900

    하이퍼파라미터 튜닝 완료

--- build_ml.py
+++ build_ml.py
@@ -2,7 +2,18 @@
 
 import os
 import pandas as pd
-from sklearn.model_selection import train_test_split
+from sklearn.model_selection import train_test_split, cross_val_score, RandomizedSearchCV
+from lightgbm import LGBMRegressor
+from sklearn.linear_model import LinearRegression
+from sklearn.ensemble import RandomForestRegressor
+from category_encoders.count import CountEncoder
+from sklearn.preprocessing import LabelEncoder, StandardScaler
+from category_encoders import OneHotEncoder
+from category_encoders.cat_boost import CatBoostEncoder
+from sklearn.impute import SimpleImputer
+from sklearn.pipeline import make_pipeline, Pipeline
+from sklearn.metrics import accuracy_score, r2_score, f1_score, mean_absolute_error, mean_squared_error, classification_report, roc_auc_score, plot_confusion_matrix
+from category_encoders.target_encoder import TargetEncoder
 
 
 CSV_FILENAME = 'Games.csv'
@@ -55,3 +66,117 @@ train = engineer(train)
 val = engineer(val)
 test = engineer(test)
 
+target = 'Added'
+
+# train, val, test셋 X, y로 분리
+X_train = train.drop(target, axis=1)
+y_train = train[target]
+X_val = val.drop(target, axis=1)
+y_val = val[target]
+X_test = test.drop(target, axis=1)
+y_test = test[target]
+
+encoder_list = ['Platform', 'Genres', 'Tag', 'Esrb_rating']
+
+# multiple linear regression
+# linear_pipe = make_pipeline(
+#     TargetEncoder(cols = encoder_list),
+#     SimpleImputer(),
+#     StandardScaler(),
+#     LinearRegression()
+# )
+
+# linear_pipe.fit(X_train, y_train)
+# y_pred = linear_pipe.predict(X_val)
+
+
+# mse = mean_squared_error(y_val, y_pred)
+# rmse = mse ** 0.5
+# mae = mean_absolute_error(y_val, y_pred)
+# r2 = r2_score(y_val, y_pred)
+
+# print('Training score: ', linear_pipe.score(X_train, y_train)) # 0.15212
+# print('Validation score: ', linear_pipe.score(X_val, y_val)) # 0.10359
+# print(f'MSE: {mse:.1f}\nRMSE: {rmse:.1f}\nMAE: {mae:.1f}\nR2: {r2:.1f}') # 1786127.8, 1336.5, 839.5, 0.1
+
+
+# randomforest model\
+
+
+rf_pipe = make_pipeline(
+    TargetEncoder(cols = encoder_list),
+    SimpleImputer(),
+    StandardScaler(),
+    RandomForestRegressor(random_state=42)
+)
+
+rf_pipe.fit(X_train, y_train)
+y_pred = rf_pipe.predict(X_val)
+
+
+mse = mean_squared_error(y_val, y_pred)
+rmse = mse ** 0.5
+mae = mean_absolute_error(y_val, y_pred)
+r2 = r2_score(y_val, y_pred)
+
+print('Training score: ', rf_pipe.score(X_train, y_train))
+print('Validation score: ', rf_pipe.score(X_val, y_val))
+print(f'MSE: {mse:.4f}\nRMSE: {rmse:.4f}\nMAE: {mae:.4f}\nR2: {r2:.4f}')
+
+# for i in list(range(2, 11)):
+#     scores = cross_val_score(rf_pipe, X_train, y_train, cv = i, scoring = 'neg_root_mean_squared_error')
+#     print(f'cv = {i}, scores = {scores}')
+#     print(f'scores mean = {scores.mean()}')
+
+
+# hyperparameter tuning
+dists = {
+    'randomforestregressor__n_estimators': [200, 300, 400, 500],
+    'randomforestregressor__criterion': ['squared_error', 'absolute_error', 'poisson'],
+    'randomforestregressor__max_depth': [2, 5, 8, 11, None],
+    'randomforestregressor__min_samples_split': [2, 4, 6, 10, None],
+    'randomforestregressor__min_samples_leaf': [2, 5, 8, 10, None],
+    'randomforestregressor__max_features': ['auto', 'sqrt', 'log2']
+}
+
+clf = RandomizedSearchCV(
+    rf_pipe,
+    param_distributions = dists,
+    n_iter = 5,
+    cv = 10,
+    verbose = 1,
+    scoring = 'neg_root_mean_squared_error',
+    n_jobs = -1
+)
+
+# clf.fit(X_train, y_train)
+
+# print('best hyperparameter: ', clf.best_params_)
+# print('best f1 score: ', clf.best_score_)
+
+new_rf_pipe = make_pipeline(
+    TargetEncoder(cols = encoder_list),
+    SimpleImputer(),
+    StandardScaler(),
+    RandomForestRegressor(
+        n_estimators = 300,
+        min_samples_split = 6,
+        min_samples_leaf = 8,
+        max_features = 'sqrt',
+        criterion ='squared_error',
+        random_state = 42
+    )
+)
+
+new_rf_pipe.fit(X_train, y_train)
+y_pred = new_rf_pipe.predict(X_val)
+
+
+mse = mean_squared_error(y_val, y_pred)
+rmse = mse ** 0.5
+mae = mean_absolute_error(y_val, y_pred)
+r2 = r2_score(y_val, y_pred)
+
+print('Training score: ', new_rf_pipe.score(X_train, y_train))
+print('Validation score: ', new_rf_pipe.score(X_val, y_val))
+print(f'MSE: {mse:.4f}\nRMSE: {rmse:.4f}\nMAE: {mae:.4f}\nR2: {r2:.4f}') # r2 score: 0.239로 좋아하는 내가 참 싫다
