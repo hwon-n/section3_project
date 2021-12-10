94efb2ad105d960b57b58ef6badae06ce5ad19ac
[hwon-n] <woo_oas@naver.com>
2021-12-10 23:30:48 +0900

    html 완료

--- flask_app/app.py
+++ flask_app/app.py
@@ -1,14 +1,21 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 
 
 
 
 app = Flask(__name__)
 
-@app.route('/')
+@app.route('/', methods=['GET', 'POST'])
 def index():
     
-    return render_template('index.html'), 200
+    if request.method == 'GET':
+        return render_template('index.html'), 200
+    elif request.method == 'POST':
+        result = request.form
+        return render_template('index.html', result=result), 200
+
 
 if __name__ == "__main__":
     app.run(debug=True)
+
+
